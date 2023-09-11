8ca28ed2af6a7ee85c3678ca758e0dd60be6e3f7
Igor Dulovic <igorx24@gmail.com>
2023-09-11 13:33:31 +0200

    implementing caching v1

--- src/providers/query.tsx
+++ src/providers/query.tsx
@@ -1,24 +1,33 @@
 import { useQuery } from "@tanstack/react-query";
 import { getCountry } from "./api";
 
+const cache: { [key: string]: any[] } = {"":[]};
+
 export const useGetCountry = (country: string) => {
   const countryKey = [["country", country]];
-  const STALE_TIME = 600000;
+  const STALE_TIME = 0;
 
+  const dataCatched = cache[country]
+  
   const {
     isLoading: countryLoading,
     error: countryError,
     data,
-  } = useQuery<boolean, Error, any[]>(countryKey, () => getCountry(country), {
-    enabled: !!country,
+  } = useQuery<boolean, Error, any>(countryKey, () => getCountry(country), {
+    enabled: !!country && !dataCatched,
     staleTime: STALE_TIME,
   });
 
-  const countryData = data?.map((country) => ({
+  cache[country] = data;
+
+  console.log(cache);
+
+  const countryData = cache[country]?.map((country:any) => ({
     area: country.area,
     name: country.name.official,
     flag: country.flags.png,
   }));
+
 
   return {
     countryLoading,
